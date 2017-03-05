ca5b979fea89605450262dfa2760b2a884230b26
XLuoChen <1425145741@qq.com>
2017-03-05 15:27:10 +0800

    update spec

--- spec/get-all-palindrome-str-spec.js
+++ spec/get-all-palindrome-str-spec.js
@@ -4,6 +4,6 @@ const getAllPalindromeStr = require('../src/get-palindrome-str');
 describe('get all palindrome string', () => {
   it('should return all palindrome string', () => {
     const result = getAllPalindromeStr('abcdef');
-    result.should.eql(['ba', 'cba', 'dcba', 'edcba', 'fedcba']);
+    result.should.eql(['fedcba', 'edcba', 'dcba', 'cba', 'ba', 'a']);
   });
 });
