dc6ec9bd04ce6b9c8235b5a21b971a85c1f88d61
XLuoChen <1425145741@qq.com>
2017-03-05 15:45:55 +0800

    add answer

--- src/get-palindrome-str.js
+++ src/get-palindrome-str.js
@@ -1,6 +1,14 @@
 function getAllPalindromeStr(str) {
   //在这里实现函数
+  const result = [];
+  let length = str.length;
+  while (length > 0) {
+    let subString = str.slice(0, length);
+    result.push(subString.split('').reverse().join(''));
+    length--;
+  }
 
+  return result;
 }
 
 module.exports = getAllPalindromeStr;
